35a8c6fc5ec28adabd7634eb6c62785a054e6837
Hanna_Khomka <Hanna_Khomka@EPBYMINW2706>
2016-06-28 12:52:02 +0300

    third commit

--- SimpleCalculation/CalculationTests/UnitTest1.cs
+++ SimpleCalculation/CalculationTests/UnitTest1.cs
@@ -3,37 +3,48 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Calculator;
 namespace CalculationTests
 {
+    
     [TestClass]
     public class CalculationTests
     {
+       
         [TestMethod]
         public void TestAddition()
         {
             Calculation calc = new Calculation();
             long a, b;
-            long.TryParse((Resource1.FirstNumber), out a);
-            long.TryParse((Resource1.FirstNumber), out b);
+            long.TryParse((Resource1.first), out a);
+            long.TryParse((Resource1.second), out b);
             Assert.AreEqual((a + b), calc.Addition(a, b) );
         }
 
+        [TestMethod]
         public void TestSubtraction()
         {
             Calculation calc = new Calculation();
             long a, b;
+            long.TryParse((Resource1.first), out a);
+            long.TryParse((Resource1.second), out b);
             Assert.AreEqual((a - b), calc.Subtraction(a, b));
         }
 
+        [TestMethod]
         public void TestMultiplication()
         {
             Calculation calc = new Calculation();
             long a, b;
+            long.TryParse((Resource1.first), out a);
+            long.TryParse((Resource1.second), out b);
             Assert.AreEqual((a * b), calc.Multiplication(a, b));
         }
 
+        [TestMethod]
         public void TestDivision()
         {
             Calculation calc = new Calculation();
             long a, b;
+            long.TryParse((Resource1.first), out a);
+            long.TryParse((Resource1.second), out b);
             Assert.AreEqual((a / b), calc.Division(a, b));
         }
     }
